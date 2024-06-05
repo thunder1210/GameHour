0cd4b866cf2a8b21855229d6310c1178e8a58f23
NEWEB\nathan.zhao <whatever12s@yahoo.com.tw>
2024-06-05 17:23:19 +0800

    Initialize gameHour Project

---
+++ src/main/java/com/thunder/gamehour/GamehourApplication.java
@@ -0,0 +1,13 @@
+package com.thunder.gamehour;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamehourApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamehourApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thunder/gamehour/GamehourApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thunder.gamehour;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamehourApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
